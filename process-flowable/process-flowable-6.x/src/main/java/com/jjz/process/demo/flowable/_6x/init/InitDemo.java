aa4d99276120c88c7ab965ea8a8ba374595f001e
jjzBruce <ExceptZhang@live.com>
2023-07-02 21:13:29 +0800

    搭建

---
+++ process-flowable/process-flowable-6.x/src/main/java/com/jjz/process/demo/flowable/_6x/init/InitDemo.java
@@ -0,0 +1,26 @@
+package com.jjz.process.demo.flowable._6x.init;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+
+/**
+ * 初始化
+ *
+ * @Author zhangj
+ * @Date 2023/7/1 21:48
+ */
+public class InitDemo {
+
+    public static void main(String[] args) {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+    }
+
+}
