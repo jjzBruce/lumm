a95ed1356954f34894f84c028831a7281c4a2ed1
jjzBruce <ExceptZhang@live.com>
2023-08-30 15:51:21 +0800

    数据安全 - MP 实现方案

--- lumm-security-data/lumm-security-data-mp/src/main/java/com/lumm/securitydata/mp/entity/User.java
+++ lumm-security-data/lumm-security-data-mp/src/main/java/com/lumm/securitydata/mp/entity/User.java
@@ -1,5 +1,6 @@
 package com.lumm.securitydata.mp.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 /**
@@ -7,6 +8,7 @@ import lombok.Data;
  * @since 1.0.0
  */
 @Data
+@TableName("`user`")
 public class User {
     private Long id;
     private String name;
