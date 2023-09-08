6f61ac23ab248e8fe8a0b2d5f24d35685a96ea27
jjzBruce <ExceptZhang@live.com>
2023-09-08 16:17:30 +0800

    整体调整

---
+++ lumm-template/lumm-security-data-mp/target/classes/db/schema-h2.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `user`;
+
+-- org.h2.jdbc.JdbcSQLSyntaxErrorException: `id` BIGINT[*](11) NOT NULL COMMENT '主键ID'
+-- https://stackoverflow.com/questions/70695039/org-h2-jdbc-jdbcsqlsyntaxerrorexception-after-h2-version-upgrade
+-- INT(11) is allowed only in MySQL and MariaDB compatibility modes, but the specified precision is ignored by H2.
+-- This definition is rejected in all other compatibility modes in H2 2.0, you need to use INT or INTEGER.
+CREATE TABLE `user`
+(
+    `id`    BIGINT  NOT NULL COMMENT '主键ID',
+    `code`  VARCHAR(30) NULL DEFAULT NULL COMMENT '编码',
+    `name`  VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
+    `age`   INT         NULL DEFAULT NULL COMMENT '年龄',
+    `email` VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',
+    `dept_code` VARCHAR(50) NULL DEFAULT NULL COMMENT '部门编码',
+    PRIMARY KEY (`id`)
+);
